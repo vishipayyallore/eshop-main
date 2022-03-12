2f9483ef6e7502305dbc5469201a25e3e4c01f1c
Viswanatha Swamy P K <vishipayyallore@yahoo.com>
2022-03-12 18:35:44 +0530

    Updating the git-pull.sh

--- Scripts/git-pull.sh
+++ Scripts/git-pull.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-REPOSITORIES=(eshop-services-products)
+REPOSITORIES=(eshop-client-ngweb eshop-services-basket eshop-services-products eshop.apigateway.ocelot)
 
 for REPOSITORY in ${REPOSITORIES[*]}
 do
 	 echo ========================================================
 	 echo Updating the repository: $REPOSITORY
 	 echo ========================================================
+
+	 if [[ ! -e $REPOSITORY ]]; then
+		epository_url="https://github.com/vishipayyallore/"$REPOSITORY".git"
+		echo $epository_url
+    	git clone $epository_url
+	 fi
 
 	 cd $REPOSITORY && git checkout && git pull && git checkout main && git pull && cd ..
 done
