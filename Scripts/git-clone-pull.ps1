10938dea10bfef7aba51c739cec8d0603db8f45d
Viswanatha Swamy P K <vishipayyallore@yahoo.com>
2022-03-16 15:13:53 +0530

    Update git-clone-pull.ps1

--- Scripts/git-clone-pull.ps1
+++ Scripts/git-clone-pull.ps1
@@ -1,23 +1,23 @@
 <#
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # Description: Shell script to clone a git repository and pull the latest changes
-# Authors: Robbie, and Swamy
+# Authors: Apaar, Robbie, and Swamy
 # Date: 12-Mar-2022
 # Modified: 13-Mar-2022
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 #>
 
-$dir = pwd
-if ( !$dir.path.EndsWith('\eshop-main\Scripts') ){
-   Write-Host "Please execute the script from (YourDrive/YourRootFolder/eshop-main/Scripts) directory" -ForegroundColor red
-   exit
+if ( ! $PWD.path.EndsWith('\eshop-main\Scripts') ) {
+    Write-Host "Please execute the script from (YourDrive:\YourRootFolder\eshop-main\Scripts) directory" -ForegroundColor red
+    
+    exit
 }
 
-while ( $pwd.path.Contains('eshop-main')) {
-    cd ..
+while ( $PWD.path.Contains('eshop-main')) {
+    Set-Location ..
 }
 
-$repositories = ("eshop-client-ngweb", "eshop-client-razorweb", "eshop-client-rjweb",  "eshop-client-webstatus", "eshop-services-basket", "eshop-services-products", "eshop-apigateway-ocelot")
+$repositories = ("eshop-client-ngweb", "eshop-client-razorweb", "eshop-client-rjweb", "eshop-client-webstatus", "eshop-services-basket", "eshop-services-products", "eshop-apigateway-ocelot")
 
 foreach ($epository in $repositories) {
 
@@ -27,5 +27,5 @@ foreach ($epository in $repositories) {
     
     $epository_url = "https://github.com/vishipayyallore/" + $epository + ".git"
     
-    git clone $epository_url
+    # git clone $epository_url
 }
