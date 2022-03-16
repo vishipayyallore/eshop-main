1ccc6e6df86d50d766e8e54a30d98f8eb5ff5a7a
Apaar Sharma <36641301+apaarshrm39@users.noreply.github.com>
2022-03-16 15:27:28 +0530

    Replicated the Logic for Powershell script (#9)
    * added warning message on powershell script
    
    * Fixes to script
    
    * fixed typo
    
    * Update git-clone-pull.ps1
    
    * Update git-clone-pull.ps1
    
    * Update git-clone-pull.ps1
    
    Co-authored-by: Viswanatha Swamy P K <vishipayyallore@yahoo.com>

--- Scripts/git-clone-pull.ps1
+++ Scripts/git-clone-pull.ps1
@@ -1,21 +1,37 @@
 <#
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # Description: Shell script to clone a git repository and pull the latest changes
-# Authors: Robbie, and Swamy
+# Authors: Apaar, Robbie, and Swamy
 # Date: 12-Mar-2022
-# Modified: 13-Mar-2022
+# Modified: 16-Mar-2022
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 #>
 
-$repositories = ("eshop-client-ngweb", "eshop-client-razorweb", "eshop-client-rjweb",  "eshop-client-webstatus", "eshop-services-basket", "eshop-services-products", "eshop-apigateway-ocelot")
+if ( ! $PWD.path.EndsWith('\eshop-main\Scripts') ) {
+    Write-Host "Please execute the script from (YourDrive:\YourRootFolder\eshop-main\Scripts) directory" -ForegroundColor red
+    
+    exit
+}
+
+while ( $PWD.path.Contains('eshop-main')) {
+    Set-Location ..
+}
+
+$repositories = ("eshop-client-ngweb", "eshop-client-razorweb", "eshop-client-rjweb", "eshop-client-webstatus", "eshop-services-basket", "eshop-services-products", "eshop-apigateway-ocelot")
 
 foreach ($epository in $repositories) {
 
     Write-Host "=========================================="
-    Write-Host "Cloning the Repository: "$epository
+    Write-Host "Updating the Repository: "$epository
     Write-Host "=========================================="
     
-    $epository_url = "https://github.com/devmentors/" + $epository + ".git"
+    if ( Test-Path -Path "$($PWD)\$epository") {
+        Set-Location $epository && git checkout && git pull && git checkout main && git pull && Set-Location ..
+    }
+    else {
+        $epository_url = "https://github.com/vishipayyallore/" + $epository + ".git"
     
-    git clone $epository_url
+        git clone $epository_url
+    }
+   
 }
