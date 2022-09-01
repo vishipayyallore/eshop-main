59a443c863308a5f65bd2e93898c2c5a89ab12c7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-09-01 14:33:47 +0530

    Few Changes

--- Infrastructure/3-backend.tf
+++ Infrastructure/3-backend.tf
@@ -1,18 +1,18 @@
 terraform {
 
-  # backend "azurerm" {
-  #   resource_group_name  = "rg-dnlh-12mar-dev"
-  #   storage_account_name = "sttfstatedhls"
-  #   container_name       = "terraformstate"
-  #   key                  = "terraformstateaks.tfstate"
-  # }
-
-  cloud {
-    organization = "swamy-the-tf-learner"
-
-    workspaces {
-      name = "First-Simple-Work-Space"
-    }
+  backend "azurerm" {
+    resource_group_name  = "rg-dnlh-12mar-dev"
+    storage_account_name = "sttfstatedhls"
+    container_name       = "terraformstate"
+    key                  = "terraformstateaks.tfstate"
   }
+
+  # cloud {
+  #   organization = "swamy-the-tf-learner"
+
+  #   workspaces {
+  #     name = "tf-workspace-for-azcloud"
+  #   }
+  # }
 
 }
