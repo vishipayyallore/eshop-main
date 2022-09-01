827bd7e5eb2f7a23558f1a796859ba3bdd5b4bee
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-09-01 13:04:20 +0530

    Few Updates

--- Infrastructure/6-aks-administrators-azure-ad.tf
+++ Infrastructure/6-aks-administrators-azure-ad.tf
@@ -1,7 +1,11 @@
+data "azuread_client_config" "current" {}
+
 # Create Azure AD Group in Active Directory for AKS Admins
 resource "azuread_group" "aks_administrators" {
-  name        = "${azurerm_resource_group.rg_eshop.name}-cluster-administrators"
-  description = "Azure AKS Kubernetes administrators for the ${azurerm_resource_group.rg_eshop.name}-cluster."
+  display_name     = "${azurerm_resource_group.rg_eshop.name}-cluster-administrators"
+  description      = "Azure AKS Kubernetes administrators for the ${azurerm_resource_group.rg_eshop.name}-cluster."
+  owners           = [data.azuread_client_config.current.object_id]
+  security_enabled = true
 }
 
 
