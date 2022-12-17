3847d3c5a42287d9ef28a22ff266c04594e13eea
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-12-17 17:36:19 +0530

    Swamy/27nov2022 session13 firstlook (#87)
    * Update Architecture.pptx
    
    * Update 8-aks_cluster.tf

--- Infrastructure/8-aks_cluster.tf
+++ Infrastructure/8-aks_cluster.tf
@@ -9,7 +9,7 @@ resource "azurerm_kubernetes_cluster" "aks_eshop_cluster" {
 
   default_node_pool {
     name                 = "systempool"
-    vm_size              = "Standard_DS2_v2"
+    vm_size              = "Standard_DS8_v3"
     orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
     # availability_zones   = [1, 2, 3]
     enable_auto_scaling = true
